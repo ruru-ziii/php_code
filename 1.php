a5feec1f4be779be5293a8b836170e0de427fb42
ruru-ziii <rozialfach@gmail.com>
2025-01-23 15:44:32 +0700

    1.php

--- 1.php
+++ 1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hitung Gaji</title>
+    <title>calculate total employee salary</title>
 </head>
 <body>
     <h1>employee salary</h1>
